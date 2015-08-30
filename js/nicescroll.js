3f3cae3591acdf27dc01a7094b05d40358f565f2
Sandeep Vattapparambil <sandeepv68@gmail.com>
2015-08-30 21:18:19 +0530

    initial template setup

---
+++ js/nicescroll.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+$(document).ready(function() {  
+        $(".side-nav").niceScroll({
+			cursorcolor:"#757575",
+			cursorborder: "1px solid #757575"
+			});
+}); 
+/****************Modal********************/
+$(document).ready(function() {  
+        $("#image_edit").niceScroll({
+			cursorcolor:"#757575",
+			cursorborder: "1px solid #757575"
+			});
+});
